839c9ce03b049e1907b3563dc79eef02ad8b4d66
Pablo Sanchez <pablosanchez@Pablos-MacBook-Air.local>
2023-08-01 10:17:07 -0600

    added php file to link to js scripts

---
+++ includes/semi-sticky-header-scripts.php
@@ -0,0 +1,9 @@
+<?php
+
+//Load JS on the front end
+function semi_sticky_header_frontend_scripts()
+{
+  wp_enqueue_script('semi-sticky-header-frontend-scripts', SEMI_STICKY_HEADER_URL . '/js/semi-sticky-header-scripts.js', [], time(), true);
+}
+
+add_action('wp_enqueue_scripts', 'semi_sticky_header_frontend_scripts');
