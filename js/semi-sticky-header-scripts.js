d8a847fa2f2f06b1de8954c83cd47f55284318d1
Pablo Sanchez <psanchezdev345@gmail.com>
2023-08-04 22:46:45 -0600

    add js and css code

--- js/semi-sticky-header-scripts.js
+++ js/semi-sticky-header-scripts.js
@@ -1,1 +1,9 @@
-alert('linked')
+const header = document.getElementsByTagName('header')
+
+//Fix header to top from start
+header.classList.add('fixed-header')
+
+//upon scroll down, hide the header (adding the 'hide' class)
+
+
+//upon scroll up, show the header (removing the 'hide' class)
